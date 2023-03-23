a8ca1843589f72a8234d7bfed37e63836c940c15
blas <blashernandez98@gmail.com>
2023-03-23 12:54:38 -0300

    Task2 docs

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+""" Task 2 Module """
+
 
 def matrix_shape(matrix: list):
+    """ Returns shape of matrix """
     out = []
     while type(matrix[0]) is list:
         out.append(len(matrix))
