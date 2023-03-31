27b80a9954db34939bc3f785acf0d7104049acfc
blas <blashernandez98@gmail.com>
2023-03-31 17:19:35 -0300

    Task4

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+student_grades.sort()
+
+# Define the bin edges for the histogram
+bins = np.arange(0, 110, 10)
+
+# Create the histogram
+hist, bin_edges = np.histogram(student_grades, bins=bins)
+
+# Create the bar plot
+plt.bar(bin_edges[:-1], hist, width=10, align="edge", edgecolor="black")
+plt.xticks(bin_edges)
+
+# Set the labels for the X-axis and Y-axis
+plt.xlabel('Grades')
+plt.ylabel('Number of students')
+plt.xlim(0, 100)
+plt.ylim(0, 30)
+
+# Show the plot
+plt.show()
