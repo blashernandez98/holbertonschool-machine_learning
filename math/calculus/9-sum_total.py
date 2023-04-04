b92ae00ddcec8a32a3bc019bdc22a7807c5cfd3e
blas <blashernandez98@gmail.com>
2023-04-04 11:19:46 -0300

    Task9-NumpyInside

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """ Task 9 module """
 
-import numpy as np
-
 
 def summation_i_squared(n):
+    import numpy as np
     """ Returns sum of squares until n """
     nums = np.arange(1, n+1)**2
     return sum(nums)
