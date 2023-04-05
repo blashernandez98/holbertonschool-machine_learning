c315659dc5d85151f18171b85768a099421e1d5c
Blas Hernández <68179343+blashernandez98@users.noreply.github.com>
2023-04-05 16:24:05 -0300

    Update 17-integrate.py

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -2,13 +2,17 @@
 """ Task 17 module """
 
 
-def poly_integral(poly, c=0):
-    """ Return derivative of poly """
+def poly_integral(poly, C=0):
+    """ Return integral of poly """
     if type(poly) is not list or len(poly) == 0:
         return None
     expo = 1
-    out = [c]
+    out = [C]
     for term in poly:
-        out.append(term / expo)
+        new_term = term / expo
+        if is_integer(new_term):
+            out.append(int(new_term))
+        else:
+            out.append(new_term)
         expo += 1
     return out
