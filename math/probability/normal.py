6aef87138d94a4557f3896d5024f37f231e8b3f8
blas <blashernandez98@gmail.com>
2023-04-07 19:54:33 -0300

    Task8

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -2,6 +2,15 @@
 """ Normal distribution module """
 
 
+pi = 3.1415926536
+e = 2.7182818285
+
+def error_func(x):
+    """ Error function """
+    ft = 2 / pi ** 0.5
+    st = x - (x ** 3 / 3) + (x ** 5 / 10) - (x ** 7 / 42) + (x ** 9 / 216)
+    return ft * st
+
 class Normal():
     """ Class representing a normal distribution """
 
@@ -24,18 +33,22 @@ class Normal():
             self.stddev = stddev ** 0.5
 
     def z_score(self, x):
-        """ Calculates the z-score of a given value """
+        """ Calculates the z-score of a given value @x """
         return (x - self.mean) / self.stddev
 
     def x_value(self, z):
-        """ Calculates the value for a given z-score """
+        """ Calculates the value for a given @z-score """
         return (z * self.stddev) + self.mean
 
     def pdf(self, x):
-        """ Calculates PDF for given x value """
-        pi = 3.1415926536
-        e = 2.7182818285
+        """ Calculates PDF for given @x value """
         first_term = 1 / (self.stddev * (pi * 2) ** 0.5)
         expo = (((x - self.mean) / self.stddev) ** 2) * -0.5
 
         return (first_term * (e ** expo))
+
+    def cdf(self, x):
+        """ Calculates the CDF for given @x value """
+        erf_x = (x - self.mean) / (self.stddev * 2 ** 0.5)
+
+        return (0.5 * (1 + error_func(erf_x)))
