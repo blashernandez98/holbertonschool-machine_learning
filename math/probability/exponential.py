afd6724d7b8a4e4b8caa35137b7dde653e37acc2
blas <blashernandez98@gmail.com>
2023-04-07 14:11:13 -0300

    Task3

---
+++ math/probability/exponential.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Exponential module """
+
+class Exponential():
+    """ Class representing Exponential distribution """
+
+    def __init__(self, data=None, lambtha=1.):
+        """ Custom init method """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = lambtha
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = 1 / (sum(data) / len(data))
