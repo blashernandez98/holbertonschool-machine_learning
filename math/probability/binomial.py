2e0d8462dcbb87ffde4a0137d2bafeaa3b0a96d7
blas <blashernandez98@gmail.com>
2023-04-07 22:15:57 -0300

    Task11

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -2,6 +2,14 @@
 """ Binomial distribution module """
 
 
+def factorial(n):
+    """ Factorial of @n """
+    res = 1
+    for i in range(1, n+1):
+        res *= i
+    return res
+
+
 class Binomial():
     """ Class representing a Binomial Distribution """
 
@@ -24,3 +32,13 @@ class Binomial():
             variance = sum((x - mean) ** 2 for x in data) / (len(data))
             self.n = round(mean / (-(variance/mean) + 1))
             self.p = mean / self.n
+
+    def pmf(self, k):
+        """ Calculates PMF for given k """
+        k = int(k)
+        if k < 0:
+            return 0
+
+        n_choose_k = factorial(self.n) / (factorial(k) * factorial(self.n - k))
+        pmf = n_choose_k * (self.p ** k) * ((1 - self.p) ** (self.n - k))
+        return pmf
