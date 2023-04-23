dc63852a9e1f12eb35dda3000adaef7f2abc015d
blas <blashernandez98@gmail.com>
2023-04-23 16:44:48 -0300

    Task17

---
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" Task 17 module """
+
+
+import numpy as np
+
+
+def int_validate(param, name):
+    """ Validates type and value for @param """
+    if type(param) is not int:
+        raise TypeError("{} must be an integer".format(name))
+    if param < 1:
+        raise ValueError("{} must be a positive integer".format(name))
+
+
+class DeepNeuralNetwork():
+    """ Represents a deep neural network """
+
+    def __init__(self, nx, layers):
+        """
+        @nx is the number of input features
+        @layers is a list representing the number
+        of nodes in each layer of the network
+        """
+
+        int_validate(nx, "nx")
+        if type(layers) is not list or len(layers) < 1\
+                or any(map(lambda x: x < 0 or type(x) is not int, layers)):
+            raise TypeError("layers must be a list of positive integers")
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        layers.insert(0, nx)
+        for i in range(1, len(layers)):
+            self.__weights["W{}".format(i)] = np.random.randn(
+                layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
+            self.__weights["b{}".format(i)] = np.zeros(shape=(layers[i], 1))
