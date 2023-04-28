83714bd79b7b4fb69aa87f8d3f5f768655cbcdb7
blas <blashernandez98@gmail.com>
2023-04-28 13:03:05 -0700

    Task 0 Imports before functions

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-def create_placeholders(nx, classes):
-    import tensorflow as tf
+import tensorflow as tf
 
+
+def create_placeholders(nx, classes):
     x = tf.placeholder(tf.float32, shape=(None, nx))
     y = tf.placeholder(tf.float32, shape=(None, classes))
     return (x, y)
