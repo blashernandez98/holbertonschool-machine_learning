4f3a6fad8dfbeed12f3347a7124223b11f66fc49
blas <blashernandez98@gmail.com>
2023-05-03 07:55:44 -0700

    Task2

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-
+""" Task 2 module """
 
 import tensorflow as tf
 
 
 def forward_prop(x, layer_sizes=[], activations=[]):
-
+    """
+    Calculates forward propagation for NN
+    Returns output tensor of last layer
+    """
     create_layer = __import__('1-create_layer').create_layer
 
     for i, l in enumerate(layer_sizes):
