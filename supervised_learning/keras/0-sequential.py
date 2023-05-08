11d68a6fe27dee750fb397109dd3330314a737bb
blas <blashernandez98@gmail.com>
2023-05-08 12:33:07 -0300

    Task0-ActivationFor1stLayer

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -10,7 +10,7 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
 
     model = k.Sequential()
     model.add(k.layers.Dense(units=layers[0], input_shape=(nx,),
-              kernel_regularizer=k.regularizers.l2(lambtha)))
+              kernel_regularizer=k.regularizers.l2(lambtha), activation=activations[0]))
     for i in range(1, len(layers)):
         model.add(k.layers.Dropout(rate=keep_prob))
         model.add(k.layers.Dense(units=layers[i], activation=activations[i],
