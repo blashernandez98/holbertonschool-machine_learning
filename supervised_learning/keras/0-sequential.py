a4efa2cfb9a76914c2b5392ad6f150f0aabb5efc
blas <blashernandez98@gmail.com>
2023-05-08 12:38:53 -0300

    Task0-CorrectKeepProb

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -10,9 +10,10 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
 
     model = k.Sequential()
     model.add(k.layers.Dense(units=layers[0], input_shape=(nx,),
-              kernel_regularizer=k.regularizers.l2(lambtha), activation=activations[0]))
+              kernel_regularizer=k.regularizers.l2(lambtha),
+              activation=activations[0]))
     for i in range(1, len(layers)):
-        model.add(k.layers.Dropout(rate=keep_prob))
+        model.add(k.layers.Dropout(rate=1-keep_prob))
         model.add(k.layers.Dense(units=layers[i], activation=activations[i],
                   kernel_regularizer=k.regularizers.l2(lambtha)))
     return model
