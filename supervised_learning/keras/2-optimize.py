48eee976c2c6ff8568f63d348a2e17ba04d0e7fa
blas <blashernandez98@gmail.com>
2023-05-11 15:01:00 -0300

    Task2-AccuracyFunc

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -19,5 +19,5 @@ def optimize_model(network, alpha, beta1, beta2):
 
     Adam = k.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2)
     network.compile(optimizer=Adam, loss="categorical_crossentropy",
-                    metrics=[k.metrics.categorical_accuracy])
+                    metrics=["accuracy"])
     return None
