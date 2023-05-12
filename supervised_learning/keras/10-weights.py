4e1a656af840fdeec1f851ed5a8621945681f304
blas <blashernandez98@gmail.com>
2023-05-12 19:33:53 -0300

    task10

---
+++ supervised_learning/keras/10-weights.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" Task 10 weights """
+
+
+import tensorflow.keras as K
+
+
+def save_weights(network, filename, save_format='h5'):
+    """
+    Saves a model's weights:
+
+    @network is the model whose weights should be saved
+    @filename is the path of the file that the weights should be saved to
+    @save_format is the format in which the weights should be saved
+    """
+    network.save_weights(filename, save_format=save_format)
+    return None
+
+
+def load_weights(network, filename):
+    """
+    Loads a model's weights:
+
+    network is the model to which the weights should be loaded
+    filename is the path of the file that the weights should be loaded from
+    """
+    network.load_weights(filename)
+    return None
