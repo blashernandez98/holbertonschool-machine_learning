8eb420300e9a530791ea9367dcc10df62db4d094
blas <blashernandez98@gmail.com>
2023-05-14 22:42:00 -0300

    Task13

---
+++ supervised_learning/keras/13-predict.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Task 13 module """
+
+
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=True):
+    """
+    Makes a prediction using a neural network:
+
+    @network is the network model to make the prediction with
+    @data is the input data to make the prediction with
+    @verbose is a boolean that determines if output should
+    be printed during the prediction process
+
+    Returns: the prediction for the data
+    """
+
+    return network.predict(data, verbose=verbose)
