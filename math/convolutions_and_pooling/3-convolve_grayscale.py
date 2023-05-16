7c204afa638f734ddaea795a9ecd58381c568b79
blas <blashernandez98@gmail.com>
2023-05-15 22:23:31 -0300

    Task3-CorrectPaddingCalc

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -35,17 +35,17 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
 
     m, h, w = images.shape
     kh, kw = kernel.shape
+    sh, sw = stride
     if padding == 'same':
-        ph = max((kh - 1) // 2, kh // 2)
-        pw = max((kw - 1) // 2, kw // 2)
+        ph = int((((h - 1) * sh + kh - h) / 2) + 1)
+        pw = int((((w - 1) * sw + kw - w) / 2) + 1)
     elif padding == 'valid':
         ph, pw = 0, 0
     else:
         ph, pw = padding
     padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), 'constant')
-    sh, sw = stride
-    ch = (h + 2 * ph - kh) // sh + 1
-    cw = (w + 2 * pw - kw) // sw + 1
+    ch = int(((h + (2 * ph) - kh) / sh) + 1)
+    cw = int(((w + (2 * pw) - kw) / sw) + 1)
     convoluted = np.zeros((m, ch, cw))
     for i in range(ch):
         for j in range(cw):
