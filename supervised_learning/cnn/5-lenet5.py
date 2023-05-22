2d0099791e1f4f18dbaccb7555432f517a89de9b
blas <blashernandez98@gmail.com>
2023-05-21 23:35:36 -0300

    Task5-FixKernelInit

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -27,25 +27,27 @@ def lenet5(X):
     Returns: a K.Model compiled to use Adam optimization
     (with default hyperparameters) and accuracy metrics
     """
+    initializer = K.initializers.he_normal()
+
     conv1 = K.layers.Conv2D(6, kernel_size=(5, 5),
                             padding='same', activation='relu',
-                            kernel_initializer='he_normal')(X)
+                            kernel_initializer=initializer)(X)
 
     pool1 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
 
     conv2 = K.layers.Conv2D(16, kernel_size=(5, 5),
                             padding='valid', activation='relu',
-                            kernel_initializer='he_normal')(pool1)
+                            kernel_initializer=initializer)(pool1)
 
     pool2 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
 
     flatten = K.layers.Flatten()(pool2)
 
     fc1 = K.layers.Dense(120, activation='relu',
-                         kernel_initializer='he_normal')(flatten)
+                         kernel_initializer=initializer)(flatten)
 
     fc2 = K.layers.Dense(84, activation='relu',
-                         kernel_initializer='he_normal')(fc1)
+                         kernel_initializer=initializer)(fc1)
 
     output = K.layers.Dense(10, activation='softmax')(fc2)
 
