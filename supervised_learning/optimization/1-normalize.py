1572cbfe60948aa5cb3db068f6071826c8b92bce
blas <blashernandez98@gmail.com>
2023-10-06 19:43:17 -0300

    Task 1

---
+++ supervised_learning/optimization/1-normalize.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" 1. Normalize """
+
+import numpy as np
+
+
+def normalize(X, m, s):
+    """
+    Function that normalizes (standardizes) a matrix
+    """
+
+    return (X - m) / s
