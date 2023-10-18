2e5c13de372d3abfb6c7ed2a7bb459a76c4ef0cc
blas <blashernandez98@gmail.com>
2023-10-18 17:37:22 -0300

    Task8

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" 8. RMSProp upgraded """
+
+import tensorflow as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """
+    Creates the training operation for a neural network in tensorflow
+    using the RMSProp optimization algorithm:
+
+    loss is the loss of the network
+    alpha is the learning rate
+    beta2 is the RMSProp weight
+    epsilon is a small number to avoid division by zero
+    Returns: the RMSProp optimization operation
+    """
+
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha,
+                                          decay=beta2,
+                                          epsilon=epsilon)
+    return optimizer.minimize(loss)
