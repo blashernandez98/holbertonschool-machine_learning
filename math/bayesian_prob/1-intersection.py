8ba664abd7c00d4c46c7db467929098c0f2e2470
blas <blashernandez98@gmail.com>
2023-11-12 20:59:58 -0300

    Task1

---
+++ math/bayesian_prob/1-intersection.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+""" Task 1. Intersection """
+
+import numpy as np
+
+
+def factorial(n):
+    """
+    Calculates the factorial of a number
+    """
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+def intersection(x, n, P, Pr):
+    """
+    Calculates the intersection of obtaining this data
+    with the various hypothetical probabilities:
+
+    x is the number of patients that develop severe side effects
+    n is the total number of patients observed
+    P is a 1D numpy.ndarray containing the various hypothetical
+    probabilities of developing severe side effects
+    Pr is a 1D numpy.ndarray containing the prior beliefs of P
+    """
+
+    if type(n) is not int or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if type(x) is not int or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if type(P) is not np.ndarray:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if type(Pr) is not np.ndarray:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    if Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    if np.any(P > 1) or np.any(P < 0):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    if np.any(Pr > 1) or np.any(Pr < 0):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
+
+    # Calculate likelihood
+    n_fact = factorial(n)
+    x_fact = factorial(x)
+    n_x_fact = factorial(n - x)
+    combination = n_fact / (x_fact * n_x_fact)
+    likelihood = combination * (P ** x) * ((1 - P) ** (n - x))
+    # Calculate intersection
+    intersection = likelihood * Pr
+    return intersection
